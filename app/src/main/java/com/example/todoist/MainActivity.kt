f8c4c03934b9f5f7cab0119463ddf3f3b225d4a8
azinalizadeh <azin98alizadeh@gmail.com>
2023-11-28 17:41:32 +0330

    Implement authentication screen (#1)

--- app/src/main/java/com/example/todoist/MainActivity.kt
+++ app/src/main/java/com/example/todoist/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.todoist
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.todoist.ui.theme.TodoistTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +26,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TodoistTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+//                val intent = Intent(
+//                    Intent.ACTION_VIEW,
+//                    Uri.parse("https://todoist.com/oauth/authorize?client_id=766ef02efd6b42429cc7a69f3e35bd3a&scope=data:read,data:delete&state=secretstring")
+//                )
+//
+//                Button(onClick = {
+//                    startActivity(intent)
+//
+//                }) {
+//                    Text(text = "Click on me")
+//                }
+
+                var showWebView by remember { mutableStateOf(false) }
+                Button(onClick = {
+
+                    showWebView = true
+
+                }) {
+                    Text(text = "Click on me")
+
+                    if (showWebView) {
+                        AndroidView(
+                            factory = { context ->
+                                WebView(context).apply {
+                                    webViewClient = WebViewClient()
+                                    loadUrl("https://todoist.com/oauth/authorize?client_id=766ef02efd6b42429cc7a69f3e35bd3a&scope=data:read,data:delete&state=secretstring")
+                                }
+                            },
+                            update = { webView ->
+                                // No-op for now
+                            },
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
                 }
+            }
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        val uri: Uri? = intent?.data
+        if (uri != null) {
+            val code = uri.getQueryParameter("code")
+            if (code != null) {
+                Log.d("AuthTestCode", "$code")
+                Toast.makeText(this, "Login success!", Toast.LENGTH_SHORT).show()
+            } else if ((uri.getQueryParameter("error")) != null) {
+                Toast.makeText(this, "Something went wrong!", Toast.LENGTH_SHORT).show()
             }
         }
     }
