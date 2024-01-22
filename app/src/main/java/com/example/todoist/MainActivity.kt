8db50ac7a6d49bf21fa8263014bcce580e62a873
alzh-azin <alzh.azin@gmail.com>
2024-01-22 17:37:08 +0330

    Implement login screen

--- app/src/main/java/com/example/todoist/MainActivity.kt
+++ app/src/main/java/com/example/todoist/MainActivity.kt
@@ -3,6 +3,22 @@ package com.example.todoist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.todoist.ui.theme.TodoistTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +29,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             TodoistTheme {
 
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
+                    ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.at_work),
+                        contentDescription = null,
+                        modifier = Modifier
+                    )
+
+                    Text(
+                        text = "Todoist",
+                        style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+                    )
+
+                    Text(
+                        text = "Designed to help you better manage your work",
+                        style = MaterialTheme.typography.labelLarge,
+                        color = Color.Gray
+                    )
+
+                    Spacer(modifier = Modifier.height(56.dp))
+
+                    Button(onClick = { }) {
+                        Text(text = "Get Started")
+                    }
+                }
 
             }
         }
