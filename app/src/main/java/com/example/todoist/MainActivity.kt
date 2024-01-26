4f6c33778d5e81203dd556c0ba67aa78fa7bde58
alzh-azin <alzh.azin@gmail.com>
2024-01-26 14:00:08 +0330

    Improve navigation logic

--- app/src/main/java/com/example/todoist/MainActivity.kt
+++ app/src/main/java/com/example/todoist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.todoist
 
+import TodoistApp
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,39 +8,10 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.todoist.ui.theme.TodoistTheme
 import dagger.hilt.android.AndroidEntryPoint
-
-
-fun openAudioSelector(): Intent {
-    val intent = Intent()
-    intent.action = Intent.ACTION_GET_CONTENT
-    intent.type = "audio/*"
-    val mimetypes = arrayOf("audio/mpeg")
-    intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes)
-    return intent
-}
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -49,93 +21,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Log.d("AuthTestCode", "onCreate")
-
         setContent {
             TodoistTheme {
+
                 navController = rememberNavController()
 
-//                Button(
-//                    modifier = Modifier.padding(top = 100.dp),
-//                    onClick = {
-//
-//                        val intent = Intent(
-//                            Intent.ACTION_VIEW,
-//                            Uri.parse("https://todoist.com/oauth/authorize?client_id=766ef02efd6b42429cc7a69f3e35bd3a&scope=data:read,data:delete&state=secretstring")
-//                        )
-//
-//                        startActivity(intent)
-//
-//                    },
-//
-//                ) {
-//                    Text(text = "Get Started")
-//                }
+                val onLogin = {
+                    val loginIntent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://todoist.com/oauth/authorize?client_id=766ef02efd6b42429cc7a69f3e35bd3a&scope=data:read,data:delete&state=secretstring")
+                    )
 
-
-                NavHost(
-                    navController = navController,
-                    startDestination = Routes.Login.route
-                ) {
-
-                    composable(Routes.Login.route) {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-
-                            ) {
-                            Column(
-                                modifier = Modifier
-                                    .weight(1f)
-                                    .fillMaxSize()
-                                    .wrapContentSize(),
-                                verticalArrangement = Arrangement.Center,
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.at_work),
-                                    contentDescription = null,
-                                )
-
-                                Text(
-                                    text = "Todoist",
-                                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
-                                )
-
-                                Text(
-                                    text = "Designed to help you better manage your work",
-                                    style = MaterialTheme.typography.labelLarge,
-                                    color = Color.Gray
-                                )
-                            }
-
-                            Spacer(modifier = Modifier.height(56.dp))
-
-                            Button(
-                                onClick = {
-
-                                    val input = Intent(
-                                        Intent.ACTION_VIEW,
-                                        Uri.parse("https://todoist.com/oauth/authorize?client_id=766ef02efd6b42429cc7a69f3e35bd3a&scope=data:read,data:delete&state=secretstring")
-                                    )
-
-                                    startActivity(input)
-                                },
-                                modifier = Modifier
-                                    .padding(bottom = 32.dp)
-
-                            ) {
-                                Text(text = "Get Started")
-                            }
-                        }
-
-                    }
-
-                    composable(Routes.Home.route) {
-                        Text(text = "This is home")
-                    }
+                    startActivity(loginIntent)
                 }
+
+                TodoistApp(
+                    navController = navController,
+                    onLogin = onLogin
+                )
+
             }
         }
     }
