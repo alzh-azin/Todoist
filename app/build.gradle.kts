6479a3fbcb9b5df0c750fb02bb5811d178b2d95a
alzh-azin <alzh.azin@gmail.com>
2024-02-05 09:49:45 +0330

    Fix manifest problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,8 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.datastore.preferences)
+
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
 }
